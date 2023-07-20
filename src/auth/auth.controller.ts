f177674c26e89b3eab1282e2e6d6c8fcaa5fa155
ArditZubaku <zubaku92@gmail.com>
2023-07-20 21:42:15 +0200

    Validation using DTOs and class-validators.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(
+    @Body() dto: AuthDto,
+    // @Body('password', ParseIntPipe) pass: string,
+  ) {
     return this.authService.signup();
   }
 
   @Post('signin')
-  signin() {
+  signin(@Body() dto: AuthDto) {
     return this.authService.signin();
   }
 }
