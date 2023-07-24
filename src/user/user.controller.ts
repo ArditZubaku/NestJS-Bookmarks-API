f1260b7ccea7bf3a2719d7bd0958d2e92f45eaec
ArditZubaku <zubaku92@gmail.com>
2023-07-24 23:40:48 +0200

    Protecting routes with that strategy.

---
+++ src/user/user.controller.ts
@@ -0,0 +1,23 @@
+import {
+  Controller,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from '../auth/guards';
+import { GetUser } from '../auth/decorator';
+import { User } from '@prisma/client';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @Get('me')
+  getMe(
+    @GetUser() user: User,
+    @GetUser('email') email: string,
+  ) {
+    console.log({
+      email,
+    });
+    return user;
+  }
+}
